e76c0cfd4010518381201086f6c6d7609bbc7417
Matheus <matheusaragaofs@gmail.com>
2021-01-06 23:58:44 -0300

    pagina de login

---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,94 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+import SignInBackground from '../../assets/sign-in-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  color: #f4ede8;
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  place-content: center; //deixa tudo no centro, horizontal e vertical, é como se fosse um justify e align-items = center juntos
+  width: 100%;
+  max-width: 700px;
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+    h1 {
+      margin-bottom: 24px;
+    }
+    input {
+      background: #232129;
+      border-radius: 10px;
+      border: 2px solid #232129; // Deixa uma borda da mesma cor pra futuramente  mudar para caso ocorra erros
+      padding: 16px;
+      color: #f4ede8;
+      width: 100%;
+      &::placeholder {
+        color: #666360;
+      }
+
+      & + input {
+        margin-top: 8px;
+      }
+    }
+    button {
+      background: #ff9000;
+      height: 56px;
+      border-radius: 10px;
+      border: 0;
+      padding: 0 16px;
+      color: #312e38;
+      font-weight: 500;
+      width: 100%;
+      margin-top: 16px;
+      transition: background-color 0.2s;
+
+      &:hover {
+        background: ${shade(0.2, '#ff9000')};
+      }
+    }
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+
+  > a {
+    // esse sinal de ">" vai estilizar as âncoras que estiverem diretamente dentro do Content, e não mais um nível pra dentro. Ele faz essa verificação
+    color: #ff9000;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+    display: flex;
+    align-items: center;
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
+
+    svg {
+      margin-right: 16px;
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${SignInBackground}) no-repeat center;
+  background-size: cover; //cobre o tamanho que sobra
+`;
